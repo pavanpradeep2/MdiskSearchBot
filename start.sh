bde8ed6ad8797b661ecbf3c7e8cd2551420a0b9f
pavanpradeep2 <107210843+pavanpradeep2@users.noreply.github.com>
2022-11-19 22:14:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
+git clone https://github.com/pavanpradeep2/MdiskSearchBot /MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
